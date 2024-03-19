67ba476f83067e4c07841ca92f9e98f0554cf667
Uchechi Nwigwe <99072641+Constance05@users.noreply.github.com>
2024-03-19 16:08:14 +0000

    Add files via upload

---
+++ helloWorld.py
@@ -0,0 +1,23 @@
+print('Hello World!')
+
+#how to print Hello World
+print('Hello World! Changed!')
+
+#Modify to accept input from user and print out data
+print('Enter your name:')
+user_input = input()
+print('Hello, ' + user_input)
+
+#Variables
+data = 5
+data_2 = "John"
+print(data)
+print(data_2)
+
+#function
+def my_function():
+  print("Hello! This is just to try a function")
+
+my_function()
+
+#updated with a new line
